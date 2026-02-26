7f9bbb5931ae4cc691f2591d33847a5930af9e28
Shubham Upadhyay <panelmaster.in@gmail.com>
2026-02-26 19:23:10 +0530

    Initial commit

--- scripts/build-php.sh
+++ scripts/build-php.sh
@@ -29,11 +29,9 @@ fi
 cd "$SRC"
 
 ########################################
-# ZLIB (SPECIAL CASE)
+# BUILD ZLIB
 ########################################
-
 echo "📦 Building zlib"
-
 cp "$GITHUB_WORKSPACE/downloads/deps/zlib-1.3.tar.gz" .
 tar -xzf zlib-1.3.tar.gz
 cd zlib-1.3
@@ -43,11 +41,9 @@ make install
 cd ..
 
 ########################################
-# ONIGURUMA
+# BUILD ONIGURUMA
 ########################################
-
 echo "📦 Building Oniguruma"
-
 cp "$GITHUB_WORKSPACE/downloads/deps/onig-6.9.9.tar.gz" .
 tar -xzf onig-6.9.9.tar.gz
 cd onig-6.9.9
@@ -57,11 +53,9 @@ make install
 cd ..
 
 ########################################
-# OPENSSL
+# BUILD OPENSSL
 ########################################
-
 echo "📦 Building OpenSSL"
-
 cp "$GITHUB_WORKSPACE/downloads/deps/openssl-3.2.1.tar.gz" .
 tar -xzf openssl-3.2.1.tar.gz
 cd openssl-3.2.1
@@ -71,11 +65,9 @@ make install_sw
 cd ..
 
 ########################################
-# ICU
+# BUILD ICU
 ########################################
-
 echo "📦 Building ICU"
-
 cp "$GITHUB_WORKSPACE/downloads/deps/icu4c-74_2-src.tgz" .
 tar -xzf icu4c-74_2-src.tgz
 cd icu/source
@@ -87,7 +79,6 @@ cd ../..
 ########################################
 # BUILD PHP
 ########################################
-
 echo "⚙ Building PHP $VERSION"
 
 cp "$GITHUB_WORKSPACE/$PHP_TARBALL" .
@@ -95,7 +86,7 @@ tar -xzf "php-$VERSION.tar.gz"
 cd "php-$VERSION"
 
 export CPPFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib -Wl,-rpath,@loader_path/../lib"
+export LDFLAGS="-L$PREFIX/lib -L/usr/lib -Wl,-rpath,@loader_path/../lib -liconv"
 export PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig"
 
 ./configure \
@@ -108,11 +99,21 @@ export PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig"
   --enable-bcmath \
   --enable-pcntl \
   --enable-sockets \
+  --enable-calendar \
+  --enable-exif \
+  --enable-fileinfo \
+  --enable-filter \
+  --enable-session \
+  --enable-tokenizer \
+  --enable-xml \
   --with-zlib="$PREFIX" \
   --with-openssl="$PREFIX" \
   --with-icu-dir="$PREFIX" \
   --with-onig="$PREFIX" \
-  --with-sqlite3
+  --with-iconv=/usr \
+  --with-sqlite3 \
+  --with-mysqli=mysqlnd \
+  --with-pdo-mysql=mysqlnd
 
 make -j$CPU
 make install
@@ -120,8 +121,7 @@ make install
 ########################################
 # ZIP
 ########################################
-
 cd "$ROOT"
 zip -r "php-$VERSION-$ARCH.zip" "php-$VERSION-$ARCH"
 
-echo "✅ Build complete"
+echo "✅ PHP $VERSION ARM64 build complete"
